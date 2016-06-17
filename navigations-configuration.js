e5e83f5948bdca8b65b2f50653b10b1ff88a315a
sumitrag-david <davisxdpfr@gmail.com>
2016-06-17 13:54:33 -0500

    created navigations-configuration file

---
+++ navigations-configuration.js
@@ -0,0 +1,10 @@
+/**
+ * Created by sumitrag-david on 17/06/16.
+ */
+var menus = [
+  {'name':'Menu 1','element':'red-object'},
+  {'name':'Menu 2','element':'blue-object'},
+  {'name':'Menu 3','element':'green-object'},
+  {'name':'Menu 4','element':'yellow-object'},
+];
+
